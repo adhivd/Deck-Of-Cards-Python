da5113aa741178d582c665be80381e21fbaccfe6
Adhiv Dhar <adhivd@gmail.com>
2018-09-30 03:13:23 -0700

    final touches

--- memory.py
+++ memory.py
@@ -3,10 +3,7 @@ from deckofcards import Deck
 import time
 import os
 import numbers
-
-# myDeck = Deck()
-# myDeck.shuffle()
-# myDeck.show()
+import sys
 
 class Game(object):
     def __init__(self):
@@ -23,8 +20,13 @@ class Game(object):
 
     def intro(self):
         validInputs = ["y","n","yes","no"]
+
+        sys.stdout.write("\x1b[8;{rows};{cols}t".format(rows=35, cols=100))
+        print("Resizing")
+        time.sleep(0.5)
+        print("Clearing")
         os.system('cls' if os.name == 'nt' else 'clear')
-        begin = input("Hi! Welcome to Memory. Would you like to begin? [y/n] \n")
+        begin = input("Hi! Welcome to Memory, the card game. Here are the rules: \n 👉🏼 The game is single player \n 👉🏼 There will a deck of cards spread out, face down, in a 4x13 grid  \n 👉🏼 Each turn, you select two cards to be face up (prompted for each card one at a time) \n 🎉 If the pair of cards has the same suit or value, they are 'matched' and stay face up \n 🚫 Else, they go face down and you move on to the next turn \n Examples: \n K♣️  and K♥️  works ✅  \n K♥️  and 4♥️  works ✅  \n K♣️  and 4♥️  does not ❌  \n 👉🏼 Your goal is to turn all the cards face up in the fewest number of turns possible \n Note: I'd highly suggest widening / heightening your terminal window \n \nWould you like to begin? [y/n] \n")
 
         while (begin not in validInputs):
             begin = input("Sorry, I didn't catch that. Would you like to begin? [y/n] \n")
@@ -55,17 +57,36 @@ class Game(object):
         while self.score != 26:
             self.showGrid()
             firstCard = self.askForRowCol()
+            if firstCard == 'god mode':
+                self.showAnswerGrid()
+                continue
+            if firstCard == 'exit':
+                self.showAnswerGrid()
+                return
             self.showGrid(firstCard)
             secondCard = self.askForRowCol(firstCard)
+            if secondCard == 'god mode':
+                self.showAnswerGrid()
+                continue
+            if secondCard == 'exit':
+                self.showAnswerGrid()
+                return
             self.showGrid(firstCard, secondCard)
             self.checkTurn(firstCard, secondCard)
             self.turns += 1
+        print("Congratulations! You win 🚀")
 
     def askForRowCol(self, firstCard=None):
-        rowcol = input("Select the row and column of the first card you'd like to turn over. Format: Row/Column (e.g. 8/a) \n")
+        if not firstCard:
+            rowcol = input("Select the row and column of the first card you'd like to turn over. Format: Row/Column (e.g. 8/a) \n")
+        else:
+            rowcol = input("Found the {}  card! Select the row and column of the next card you'd like to turn over. Format: Row/Column (e.g. 8/a) \n".format(self.answerGrid[firstCard[0]][firstCard[1]]))
+
         validInputs = ["a","b","c","d"]
         doneChecking = False
         while not doneChecking:
+            if rowcol == "god mode" or rowcol == "exit":
+                return rowcol
             if "/" not in rowcol[:]:
                 rowcol = input("Please make sure to include a / in your format between Row and Column (e.g. 8/a) \n")
                 continue
@@ -117,11 +138,11 @@ class Game(object):
         c2 = self.answerGrid[secondCard[0]][secondCard[1]]
 
         if c1.value == c2.value or c1.suit == c2.suit:
-            print("🎉 Congrats! You've matched the {} and {} cards. Moving on to next turn".format(c1,c2))
+            print("🎉 Congrats! You've matched the {}  and {}  cards. Moving on to next turn".format(c1,c2))
             self.score += 1
             time.sleep(3)
         else:
-            print("🚫 Incorrect Match! The pair you've selected will be turned face down in a few seconds . . .")
+            print("🚫 Incorrect Match! The pair you've selected ({}  and {}  ) will be turned face down in a few seconds . . .".format(c1,c2))
             self.userGrid[firstCard[0]][firstCard[1]] = "🃏"
             self.userGrid[secondCard[0]][secondCard[1]] = "🃏"
             time.sleep(4)
@@ -129,13 +150,12 @@ class Game(object):
 
     def showGrid(self, firstCard=None, secondCard=None):
         os.system('cls' if os.name == 'nt' else 'clear')
-        print("Turn {}  |  Pairs matched: {} ".format(self.turns, self.score))
+
         row = 1
-        col = 1
         # for c in range(4):
         #     print
-        print("      col")
-        print("row   a  b  c  d")
+        print("      columns")
+        print("row   a   b   c   d")
         if firstCard:
             self.userGrid[firstCard[0]][firstCard[1]] = self.answerGrid[firstCard[0]][firstCard[1]]
         if secondCard:
@@ -156,9 +176,25 @@ class Game(object):
             print("\n")
             row += 1
 
+        print("Turn {}  |  Pairs matched: {}  \nType 'god mode' to see the answers | Type 'exit' to exit \n ".format(self.turns, self.score))
 
+    def showAnswerGrid(self):
+        row = 1
 
+        print("      columns")
+        print("row   a  b  c  d")
+        for r in self.answerGrid:
+            if(row < 10):
+                print('{}     '.format(row), end='')
+            else:
+                print('{}    '.format(row), end='')
 
-
+            for c in r:
+                if(c == "🃏"):
+                    print("{}  ".format(c), end='')
+                else:
+                    print("{}  ".format(c), end='')
+            print("\n")
+            row += 1
 
 game = Game()
